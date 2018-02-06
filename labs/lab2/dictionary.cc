f71c684b62769d12551acbb30d19e824dcea5742
antongoransson <anton.goransson95@gmail.com>
2018-02-06 14:55:49 +0100

    Lab2 cleanup

--- labs/lab2/dictionary.cc
+++ labs/lab2/dictionary.cc
@@ -5,13 +5,15 @@
 #include "word.h"
 #include "dictionary.h"
 
-using std::string;
 using std::ifstream;
-using std::vector;
+using std::stringstream;
 using std::cout;
 using std::endl;
-using std::stringstream;
+using std::string;
+using std::vector;
 using std::min;
+using std::pair;
+using std::make_pair;
 
 Dictionary::Dictionary() {
 	string line, trigram, word;
@@ -23,7 +25,7 @@ Dictionary::Dictionary() {
 			 dict.insert(word);
 			 ss >> trigram;
 			 vector<string> trigrams;
-			 while(ss >> trigram)
+			 while (ss >> trigram)
 				 trigrams.push_back(trigram);
 			 words[word.length()].push_back(Word(word, trigrams));
 		 }
@@ -33,8 +35,7 @@ Dictionary::Dictionary() {
 }
 
 bool Dictionary::contains(const string& word) const {
-	const auto got = dict.find (word);
-	return got != dict.end();
+	return dict.count(word);
 }
 
 vector<string> Dictionary::get_suggestions(const string& word) const {
@@ -46,58 +47,55 @@ vector<string> Dictionary::get_suggestions(const string& word) const {
 }
 
 void Dictionary::add_trigram_suggestions(vector<string>& suggestions, const string& word) const {
-	string::size_type i, nbr_trigrams, len = word.length();
+	string::size_type i, len {word.length()};
 	if (len < 3)
 		return;
-	nbr_trigrams = len - 2;
+	string::size_type nbr_trigrams {len - 2};
 	vector<string> trigrams(nbr_trigrams);
 
 	// Create trigrams for word
-	for(string::size_type i = 0; i < nbr_trigrams; i++)
+	for (i = 0; i < nbr_trigrams; i++)
 			trigrams[i] = word.substr(i, 3);
 	sort(trigrams.begin(), trigrams.end());
 
 	// Loop through words on word.length +- 1
-	for(i = len - 1; i <= len + 1; ++i) {
-		for(const auto w: words[i]) {
-			if(w.get_matches(trigrams) >= nbr_trigrams / 2 )
+	for (i = len - 1; i <= len + 1 && i <= WORD_SIZE; ++i) {
+		for (const auto w: words[i]) {
+			if (w.get_matches(trigrams) >= nbr_trigrams / 2)
 				suggestions.push_back(w.get_word());
 		}
 	}
 }
 
 void Dictionary::rank_suggestions(vector<string>& suggestions, const string& word) const {
-	string::size_type i, j, word_len = word.length();
-	if (word_len < 3)
+	string::size_type i, j, w_len {word.length()};
+	if (w_len < 3)
 		return;
-	vector<string> suggestion_ranks[WORD_SIZE];
+		
+	vector<pair<int, string>> distances;
 	for(const auto& s: suggestions) {
 		string::size_type s_len = s.length();
+		// Initialize matrix
 		int d[26][26];
 		d[0][0] = 0;
-		//d ( i, 0 ) = i
-		for (i = 1; i <= word_len; ++i)
-			d[i][0] = i;
-		//d (0, j ) = j
-		for (j = 1; j <= s_len; ++j)
-			d[0][j] = j;
-		for (i = 1; i <= word_len; ++i) {
+		for (i = 1; i <= w_len; ++i) d[i][0] = i;
+		for (j = 1; j <= s_len; ++j) d[0][j] = j;
+		// Calculate cost using Levenshtein distance
+		for (i = 1; i <= w_len; ++i) {
 			for (j = 1; j <= s_len; ++j) {
-				d[i][j] = min({ word[i-1] == s[j-1] ? d[i-1][j-1] : d[i-1][j-1] + 1, d[i-1][j] + 1,	d[i][j-1] + 1 });
+				d[i][j] = min({ d[i-1][j-1] + (word[i-1] == s[j-1] ? 0 : 1), d[i-1][j] + 1,	d[i][j-1] + 1 });
 			}
 		}
-		suggestion_ranks[d[word_len][s_len]].push_back(s);
+		distances.push_back(make_pair(d[w_len][s_len], s));
 	}
-	j = 0;
-	for(i = 0; i != WORD_SIZE; ++i) {
-		sort(suggestion_ranks[i].begin(), suggestion_ranks[i].end());
-		for(const auto& w: suggestion_ranks[i]) {
-			suggestions[j] = w;
-			j++;
-		}
+	sort(distances.begin(), distances.end());
+	suggestions.clear();
+	for (auto s: distances) {
+		suggestions.push_back(s.second);
 	}
 }
-void Dictionary::trim_suggestions(vector<std::string>& suggestions) const{
-	int size = suggestions.size() >= 5 ? 5 : suggestions.size();
+
+void Dictionary::trim_suggestions(vector<std::string>& suggestions) const {
+	size_t size = suggestions.size() >= 5 ? 5 : suggestions.size();
 	suggestions = vector<string>(suggestions.begin(), suggestions.begin() + size);
 }
