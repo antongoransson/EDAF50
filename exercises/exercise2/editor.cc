0bfaa15ee623bdba2c7e592cc089ae58ac4b27d7
antongoransson <anton.goransson95@gmail.com>
2018-02-10 21:53:41 +0100

    Cleaner solution

--- exercises/exercise2/editor.cc
+++ exercises/exercise2/editor.cc
@@ -7,27 +7,20 @@ using std::string;
 string::size_type Editor::find_left_par(string::size_type pos) const {
 	char type;
 	char in = text[pos];
-	string::size_type number_right = 0, number_left = 0, total= 0;
+	string::size_type number_left = 0;
 	switch (in) {
 		case ']': type = '['; break;
 		case '}': type = '{'; break;
 		case ')': type = '('; break;
 		default: return -1; break;
 	}
-	for (string::size_type i = 0; i < text.length(); i++) {
-		char c = text[i];
-		if (c == type)
-			total++;
-		if (c == in && i <= pos)
-			number_right++;
+	for (string::size_type i = pos - 1; i > 0; --i) {
+		if (text[i] == in)
+			++number_left;
+		else if (text[i] == type) {
+			if (number_left == 0) return i;
+			--number_left;
+		}
 	}
-
-	for (string::size_type i = 0; i < text.length(); i++) {
-		if (text[i] == type)
-			number_left++;
-		if (total - number_right + 1 == number_left)
-			return i;
-	}
-
-	return -1;
+	return -1	;
 }
