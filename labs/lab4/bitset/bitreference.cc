3154c5a58248417459ee40c63e912d7d81f207be
antongoransson <anton.goransson95@gmail.com>
2018-02-13 14:48:01 +0100

    Lab4 A3 done

--- labs/lab4/bitset/bitreference.cc
+++ labs/lab4/bitset/bitreference.cc
@@ -2,25 +2,23 @@
 #include "bitset.h"
 
 BitReference& BitReference::operator=(bool b) {
-	//
-	// *** IMPLEMENT ***
-	// This corresponds to the set() function in SimpleBitset.
-	//
+	if (b) {
+		*p_bits |= 1L << pos;
+	} else {
+		*p_bits &= ~ (1L << pos);
+	}
 	return *this;
 }
 
 BitReference& BitReference::operator=(const BitReference& rhs) {
-	//
-	// *** IMPLEMENT ***
-	// Same as operator=(bool), but the bit is picked from rhs
-	//
+	if (rhs.p_bits[rhs.pos]) {
+		*p_bits |= 1L << pos;
+	} else {
+		*p_bits &= ~ (1L << pos);
+	}
 	return *this;
 }
 
 BitReference::operator bool() const {
-	//
-	// *** IMPLEMENT ***
-	// This corresponds to the get() function in SimpleBitset.
-	//
-	return true;
+	return (*p_bits & (1L << pos)) != 0;
 }
